750d3798549bf0b571d864f3a124c9fcfbc0d033
Sajidali Mansuri <110292563+ProgramWithSajid@users.noreply.github.com>
2023-10-04 13:39:50 +0530

    settings.py

--- project/settings.py
+++ project/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'ci!=nl9#v4x=ve#$($=!h=bd9t)kl&*1_e)v)^_ln3d2g#pqqj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
